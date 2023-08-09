4b2686971b81e9df7a1b68c7f08c51bf2b021438
mreffatti <61008357+mreffatti@users.noreply.github.com>
2023-08-09 17:35:05 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,4 +1,3 @@
-//desc: skills.js is a file that contains all the skills for copilot.
-function CalculateNumbers (var1, var2){
+function calculateNumbers (var1, var2){
     console.log(var1 + var2);
 }
